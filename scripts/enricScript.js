000e594517484a3a5171be564a898a9eee7b4f6a
Enric Broto Hernandez <enrique7siete@gmail.com>
2023-07-21 11:45:25 +0200

    (add): make game containers & main navigation

--- scripts/enricScript.js
+++ scripts/enricScript.js
@@ -1,6 +1,37 @@
-const memoryGame__button = document.querySelector('.memoryGame__button')
 
+const mainPage = document.getElementById('mainPage')
 
-tenClick__button.addEventListener('click', () => {
-    memoryGame__button.style.backgroundColor = 'red'
+const hangmanGame__selectGameButton = document.querySelector('.hangmanGame__selectGameButton')
+const hangmanGame__container = document.querySelector('.hangmanGame__container')
+const hangmanGame__Main__backButton = document.querySelector('.hangmanGame__Main__backButton')
+const hangmanGame__Main__playButton = document.querySelector('.hangmanGame__Main__playButton')
+
+/** open game **/  
+hangmanGame__selectGameButton.addEventListener('click', () => {
+    openGame()
 })
+
+function openGame() {
+    mainPage.style.display = 'none'
+    hangmanGame__container.style.display = 'Block'
+    hangmanGame__selectGameButton.style.backgroundColor = 'red'
+}
+
+hangmanGame__Main__backButton.addEventListener('click', () => {
+    goMainPage()
+})
+function goMainPage() {
+    mainPage.style.display = 'block'
+
+    hangmanGame__container.style.display = 'none'
+    hangmanGame__selectGameButton.style.backgroundColor = 'green'
+}
+
+hangmanGame__Main__playButton.addEventListener('click', () => {
+    playGame()
+})
+function playGame() {
+    hangmanGame__Main__playButton.style.backgroundColor = 'green'
+}
+
+mainPage.style.backgroundColor = 'lightblue'
