e2c45c79ba6055d4676a7a5bce0afd3526cca1de
Ian Pares <yannpares@gmail.com>
2023-07-21 14:14:11 +0200

    (add) set button fucntions

--- scripts/yannScript.js
+++ scripts/yannScript.js
@@ -9,14 +9,65 @@ const juego1 = document.getElementById('juego1');
 const juego2 = document.getElementById('juego2');
 const juego3 = document.getElementById('juego3');
 const juego4 = document.getElementById('juego4');
-// juego1.style.display = 'none';
+juego1.style.display = 'none';
 juego2.style.display = 'none';
 juego3.style.display = 'none';
 juego4.style.display = 'none';
-// initialFrame.style.display = 'none';
-mainPage.style.display = 'none';
+initialFrame.style.display = 'none';
+// mainPage.style.display = 'none';
 startGame.style.display = 'none';
 game.style.display = 'none';
 replay.style.display = 'none'; 
 level.style.display = 'none';
 hard.style.display = 'none';
+
+const tenClick__button = document.querySelector('.tenClickGame__button');
+tenClick__button.addEventListener('click', () => {
+juego1.style.display = 'block';
+initialFrame.style.display = 'grid';
+mainPage.style.display = 'none';
+});
+
+const playingButton = document.getElementById('playingButton');
+playingButton.addEventListener('click', () => {
+initialFrame.style.display = 'none';
+startGame.style.display = 'grid';
+});
+const startingButton = document.getElementById('startingButton');
+startingButton.addEventListener('click', () => {
+startGame.style.display = 'none';
+game.style.display = 'grid';
+});
+// set interval to 10 sec. Moment button action.
+const gameButton = document.getElementById('gameButton');
+gameButton.addEventListener('click', () => {
+game.style.display = 'none';
+replay.style.display = 'grid'; 
+});
+// set interval to 10 sec. Moment button action.
+const backHomeButton = document.getElementById('backHomeButton');
+backHomeButton.addEventListener('click', () => {
+replay.style.display = 'none'; 
+initialFrame.style.display = 'grid';
+});
+const playAgainButton = document.getElementById('playAgainButton');
+playAgainButton.addEventListener('click', () => {
+replay.style.display = 'none'; 
+level.style.display = 'grid';
+});
+const sameLevelButton = document.getElementById('sameLevelButton');
+sameLevelButton.addEventListener('click', () => {
+level.style.display = 'none';
+startGame.style.display = 'grid';
+});
+const harderLevelButton = document.getElementById('harderLevelButton');
+harderLevelButton.addEventListener('click', () => {
+level.style.display = 'none';
+hard.style.display = 'grid';
+});
+const gameButtonHard = document.getElementById('gameButtonHard');
+gameButtonHard.addEventListener('click', () => {
+hard.style.display = 'none';
+replay.style.display = 'grid';
+// set timer and random
+});
